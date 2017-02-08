876d90053a93a08a0be2172de8943f89cff1888b
Melvin1295 <diazrojasmelvinalexis@gmail.com>
2017-02-08 09:30:32 -0500

    ejemplo

---
+++ registro/login/home/home.controller.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['UserService', '$rootScope','$scope','$log'];
+    function HomeController(UserService, $rootScope,$scope,$log) {
+        var vm = this;
+
+        $scope.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+
+        initController();
+
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                   $log.log(user);
+                  // alert("hola"+user.nombres);
+                    $scope.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+        }
+    }
+
+})();
