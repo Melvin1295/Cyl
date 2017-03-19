88e2034d08a4abba77671e9a8995409641dac6c7
Melvin1295 <diazrojasmelvinalexis@gmail.com>
2017-03-18 19:01:46 -0500

    alll

---
+++ cylempres/indexEmpresa.php
@@ -0,0 +1,62 @@
+
+	
+<body>
+
+	<!-- Top Bar -->
+	<section class="top-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+					<div class="row">
+						<div class="col-xs-6 cuaEmpresa">
+							<img src="img/logo.png" width="80">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /Top Bar -->
+
+	<nav class="navbar navbar-default navbar-fixed-bottom">
+	  <div class="container-fluid">
+	  	<p class="navbar-text">
+	  		<a href="indexEmpresa.php" class="navbar-user">
+	    		<span class="fa fa-building-o" style="text-align: center; font-size: 25px; letter-spacing: 3px" /> Softbarz</a>
+	  	</p>
+	    <p class="navbar-text navbar-right">
+	    	<a href="#!/vacantes" class="navbar-home home">
+	    		<span class="fa fa-home" style="text-align: center; font-size: 15px" /><br/>INICIO</a>
+	    	<a href="vacantesEmpresa.php" class="navbar-link file">
+	    		<span class="fa fa-file-o" style="text-align: center; font-size: 15px" /><br/>MIS VACANTES</a>
+	    	<a href="publicarEmpresa.php" class="navbar-link edit">
+	    		<span class="fa fa-edit" style="text-align: center; font-size: 15px" /><br/>PUBLICAR UNA VACANTE</a>
+	    	<a href="candidatosEmpresa.php" class="navbar-link search">
+	    		<span class="fa fa-search" style="text-align: center; font-size: 15px" /><br/>BUSCAR CANDIDATOS</a>
+	    	<a href="configuracionEmpresa.php" class="navbar-link gears">
+	    		<span class="fa fa-gears" style="text-align: center; font-size: 15px" /><br/>CONFIGURACIÓN</a>
+	    	<a href="soporteEmpresa.php" class="navbar-link comments">
+	    		<span class="fa fa-comments" style="text-align: center; font-size: 15px" /><br/>SOPORTE</a>
+	    </p>
+	  </div>
+	</nav>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-9 avisos">
+				<div class="col-sm-4">
+					<p class="aviPubAct">Avisos publicados activos</p>
+				</div>
+				<div class="col-sm-4">
+					<p class="aviPubAct">Avisos destacados</p>
+				</div>
+				<div class="col-sm-4">
+					<p class="aviPubAct">Avisos urgentes</p>
+				</div>
+			</div>
+			<div class="col-sm-3 estadisticas"></div>
+		</div>
+	</div>
+
+</body>
+
